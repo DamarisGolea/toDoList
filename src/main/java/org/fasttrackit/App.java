f6d192a65b6fcfdcbb41b9af1310a81df9b530d3
Your Name <damaris.golea@gmail.com>
2019-08-07 20:13:26 +0300

    added repository methods for CRUD operations

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,13 +1,29 @@
 package org.fasttrackit;
 
+import org.persistence.ToDoItemRepository;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws SQLException, IOException, ClassNotFoundException {
+
+        ToDoItemRepository toDoItemRepository = new ToDoItemRepository();
+        //toDoItemRepository.createToDoItem("Learn JDBC", LocalDateTime.now().plusMonths(6));
+
+        //toDoItemRepository.deleteToDoItem(3);
+
+        System.out.println(toDoItemRepository.getToDoItems());
+        toDoItemRepository.updateToDoItem(1, true);
+
+        System.out.println(toDoItemRepository.getToDoItems());
     }
+
+
 }
--- src/main/java/org/persistence/ToDoItemRepository.java
+++ src/main/java/org/persistence/ToDoItemRepository.java
@@ -1,10 +1,17 @@
 package org.persistence;
 
+import org.fasttrackit.domain.ToDoItem;
+
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ToDoItemRepository {
 
@@ -17,6 +24,55 @@ public class ToDoItemRepository {
         ) {
             preparedStatement.setString(1, description);
             preparedStatement.setDate(2, java.sql.Date.valueOf(deadline.toLocalDate()));
+
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public List<ToDoItem> getToDoItems() throws SQLException, IOException, ClassNotFoundException {
+        String query = "SELECT id, description, deadline, done FROM to_do_item";
+
+        try(Connection connection = DatabaseConfiguration.getConnection();
+            Statement statement = connection.createStatement()
+        ){
+            ResultSet resultSet = statement.executeQuery(query);
+
+            List<ToDoItem> toDoItems = new ArrayList<>();
+
+            while (resultSet.next()){
+                ToDoItem item = new ToDoItem();
+                item.setId( resultSet.getLong("id"));
+                item.setDescription(resultSet.getString("description"));
+                item.setDeadline(resultSet.getDate("deadline").toLocalDate().atStartOfDay());
+                item.setDone(resultSet.getBoolean("done"));
+
+                toDoItems.add(item);
+            }
+            return toDoItems;
+        }
+    }
+
+    public void deleteToDoItem(long id) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "DELETE FROM to_do_item WHERE id = ?";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement= connection.prepareStatement(sql)
+        ){
+            preparedStatement.setLong(1, id);
+
+            preparedStatement.executeUpdate();
+        }
+
+    }
+
+    public void updateToDoItem (long id, boolean done) throws SQLException, IOException, ClassNotFoundException {
+        String sql = "UPDATE to_do_item SET done= ? WHERE id= ?";
+
+        try (Connection connection = DatabaseConfiguration.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ){
+            preparedStatement.setBoolean (1, done);
+            preparedStatement.setLong (2, id);
 
             preparedStatement.executeUpdate();
         }
